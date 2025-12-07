3091027916037bb7461d583b8ec6f2e03d9fa5f7
didaque-d <diego.dads52@gmail.com>
2025-12-07 16:04:10 +0000

    Adicionei pib e densidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,13 +8,13 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
     char estado, codigo[20], cidade[20];
     int populacao, turismo;
-    float area, pib;
+    float area, pib, densidade, capita;
 
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  //cidade 2
+  //cidade 2clear
     char estado2, codigo2[20], cidade2[20];
     int populacao2, turismo2;
-    float area2, pib2;
+    float area2, pib2, densidade2, capita2;
   // Área para entrada de dados
   // carta 1
     printf("Carta 1\n");
@@ -30,8 +30,17 @@ int main() {
     scanf("%f", &area);
     printf("Insira o PIB: \n");
     scanf("%f", &pib);
-    printf("Insira o numero de pontos turisticos: ");
+    printf("Insira o numero de pontos turisticos: \n");
     scanf("%d", &turismo);
+
+    //Parte 2
+    //Adicionando os calculos do nivel intermediario
+    //Dividindo a populacao pela area
+    densidade = populacao / area;
+    //dividindo pib pela populacao
+    capita = pib / populacao;
+
+
   // Área para exibição dos dados da cidade
   //carta 1
     printf("Dados da Carta 1\n");
@@ -41,7 +50,12 @@ int main() {
     printf("Populacao: %d \n", populacao);
     printf("Area: %f \n", area);
     printf("PIB: %f \n", pib);
-    printf("Pontos Turisticos: %f \n", turismo);
+    printf("Pontos Turisticos: %d \n", turismo);
+    
+    //saida da densidade
+    printf("Densidade pupulacional: %f \n", densidade);
+    //saida do pib per capita
+    printf("PIB per Capita: %f \n", capita);
 
   // Área para entrada de dados
   // carta 2
@@ -58,8 +72,15 @@ int main() {
     scanf("%f", &area2);
     printf("Insira o PIB: \n");
     scanf("%f", &pib2);
-    printf("Insira o numero de pontos turisticos: ");
+    printf("Insira o numero de pontos turisticos: \n");
     scanf("%d", &turismo2);
+
+    //Adicionando os calculos do nivel intermediario
+    //Dividindo a populacao pela area
+    densidade2 = populacao / area;
+    //dividindo pib pela populacao
+    capita2 = pib / populacao;
+   
   // Área para exibição dos dados da cidade
   //carta 2
     printf("Dados da Carta 2\n");
@@ -69,6 +90,12 @@ int main() {
     printf("Populacao: %d \n", populacao2);
     printf("Area: %f \n", area2);
     printf("PIB: %f \n", pib2);
-    printf("Pontos Turisticos: %f \n", turismo2);
+    printf("Pontos Turisticos: %d \n", turismo2);
+    
+    //saida da densidade
+    printf("Densidade pupulacional: %f \n", densidade2);
+    //saida do pib per capita
+    printf("PIB per Capita: %f \n", capita2);
+ 
 return 0;
 } 
